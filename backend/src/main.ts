04dd2f23224a95836a6b63730a41b48b2e396783
jaiganesh <jaigokul67@gmail.com>
2026-06-10 14:02:20 +0530

    page is loading image and ai summary is not implemented and modal resizing to be done

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
